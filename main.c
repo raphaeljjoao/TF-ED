ae456ea05a03cec09639bf41ba5cd47710481ac6
raphaeljjoao <raphaeljoao02@gmail.com>
2022-04-27 18:10:03 -0300

    Remoção de trechos desnecessários da main

--- main.c
+++ main.c
@@ -1,19 +1,13 @@
 #include <stdio.h>
-//#include <stdlib.h>
-//#include <time.h>
-#include "src/arvores/ABP.h"
-#include "src/arvores/AVL.h"
-//#include "src/utils.h"
 #include "src/options.h"
-//#include "src/stats.h"
 #include "src/analises/ordenada_ABP.h"
 
 
 int main(){
     printf("(INF01203) Estruturas de Dados - Trabalho Final 2021/2\n\n");
 
-    printf("ABP (%d)\n\n", sizeof(ABP));
-    analiseOrdenadaABP(TIER_4 * 1);
+    printf("ABP Ordenada\n");
+    analiseOrdenadaABP(TIER_4 * 100);
 
     return 0;
 }
