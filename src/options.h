5d6c6568061aafe961e65d4e8e0543fe4e6d3bd9
raphaeljjoao <raphaeljoao02@gmail.com>
2022-05-08 04:57:10 -0300

    Opção para impressão de dados em formato de tabela

--- src/options.h
+++ src/options.h
@@ -1,9 +1,16 @@
+#ifndef OPTIONS_H_
+
+#define OPTIONS_H_
+
 #define FALSE 0
 #define TRUE 1
 
+#define CSVMODE FALSE
 #define SILENT FALSE
 
 #define TIER_1 100
 #define TIER_2 500
 #define TIER_3 1000
 #define TIER_4 10000
+
+#endif // OPTIONS_H_
